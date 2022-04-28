91fbbf0824034382fe7f9a3f979b2992a6b256fd
NikiGorbunov <97124792+NikiGorbunov@users.noreply.github.com>
2022-04-29 01:16:00 +0300

    work 3.4

--- GameSliderSwiftUI/ContentView.swift
+++ GameSliderSwiftUI/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currentValue = Float.random(in: 0...100)
+    
+    @State private var alertIsPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 30) {
+            Text("Подвиньте слайдер, как можно ближе к: \(targetValue)")
+            
+            HStack {
+                Text("0")
+                DecimalSlider(SliderValue: $currentValue, alphaThumb: getAlpha())
+                    .onChange(of: currentValue){ newValue in
+                        currentValue = newValue
+                    }
+                    .alert("Твой счет", isPresented: $alertIsPresented, actions: {}) {
+                        Text("\(computeScore())")
+                    }
+                Text("100")
+            }
+            Button("Проверь меня") {
+                showAlert()
+            }
+            Button("Начать заново") {
+                targetValue = Int.random(in: 0...100)
+                withAnimation {
+                    currentValue = Float.random(in: 0...100)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+
+extension ContentView {
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(Double(currentValue)))
+        return 100 - difference
+    }
+    
+    private func getAlpha() -> CGFloat {
+        CGFloat(Double(computeScore()) / 100.0)
+    }
+    
+    private func showAlert() {
+        alertIsPresented.toggle()
     }
 }
 
--- GameSliderSwiftUI/DecimalSlider.swift
+++ GameSliderSwiftUI/DecimalSlider.swift
@@ -8,28 +8,53 @@
 import SwiftUI
 
 struct DecimalSlider: UIViewRepresentable {
-    @Binding var value: Double
+    
+    @Binding var SliderValue: Float
+    let alphaThumb: CGFloat
     
     func makeUIView(context: Context) -> UISlider {
         let sliderView = UISlider()
         sliderView.minimumValue = 0
         sliderView.maximumValue = 100
         sliderView.backgroundColor = .gray
-        sliderView.minimumTrackTintColor = .black
-        sliderView.maximumTrackTintColor = .black
-        sliderView.thumbTintColor = .red
+        sliderView.tintColor = .black
         sliderView.layer.cornerRadius = 10
+        
+        sliderView.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.setSliderValue),
+            for: .allTouchEvents
+        )
         
         return sliderView
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = SliderValue
+        uiView.thumbTintColor = .red.withAlphaComponent(alphaThumb)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $SliderValue)
+    }
+}
+
+extension DecimalSlider {
+    class Coordinator: NSObject {
+        @Binding var value: Float
         
+        init(value: Binding<Float>) {
+            self._value = value
+        }
+        
+        @objc func setSliderValue(_ sender: UISlider) {
+            value = sender.value
+        }
     }
 }
 
 struct DecimalSlider_Previews: PreviewProvider {
     static var previews: some View {
-        DecimalSlider(value: .constant(50))
+        DecimalSlider(SliderValue: .constant(50), alphaThumb: 0.5)
     }
 }
